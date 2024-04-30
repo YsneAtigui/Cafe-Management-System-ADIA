b5cbbc9d712bb01339563876a5dffc7705ab26dd
YassineAtigui <atiguiyassine03@gmail.com>
2024-04-30 13:28:19 +0100

    Category page

--- src/dao/Tables.java
+++ src/dao/Tables.java
@@ -13,8 +13,10 @@ public class Tables {
         try{
             String userTable = "create table user(id int AUTO_INCREMENT primary key, name varchar(200),email varchar(200),mobileNumber varchar(10),password varchar(200),securityQuestion varchar(200),answer varchar(200),status varchar(20),UNIQUE(email))";
             String adminDetails= "insert into user(name,email,mobileNumber,password,securityQuestion,answer,status) values('Admin','admin@gmail.com','1234567890','admin','admin','admin','true')";
+            String categoryTable = "create table category(id int AUTO_INCREMENT primary key, name varchar(200))";
             DbOperation.setDataOrDelete(userTable, "User Table Created Successfully");
             DbOperation.setDataOrDelete(adminDetails, "Admin Created Successfully");
+            DbOperation.setDataOrDelete(categoryTable, "Category Table Created Successfully");
         }
         catch(Exception e){
             JOptionPane.showMessageDialog(null, e);
